46dba1e12baad98d89266e1d42a7f8d957ab411b
arunsekar <arunsekar@CLAYFIN-LP303.clayfin.net>
2025-09-13 18:32:31 +0530

    Commit the logout changes file also

--- ParaBank_Selenium/src/main/java/com/Arun/Practice/Login_ParaBank.java
+++ ParaBank_Selenium/src/main/java/com/Arun/Practice/Login_ParaBank.java
@@ -1,9 +1,35 @@
 package com.Arun.Practice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Login_ParaBank {
 
-	public static void main(String[] args) {
-		System.out.println("Hii");
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://parabank.parasoft.com/parabank/index.htm");
+		
+		WebElement enter_Username = driver.findElement(By.xpath("//input[@name='username']"));
+		
+		enter_Username.sendKeys("john");
+		
+		WebElement enter_Password = driver.findElement(By.xpath("//input[@name='password']"));
+		
+		enter_Password.sendKeys("demo");
+		
+		WebElement click_Login_Button = driver.findElement(By.xpath("//input[@value='Log In']"));
+		
+		click_Login_Button.click();
+		
+		Thread.sleep(3000);
+		
+		
+		
 
 	}
 
---
+++ ParaBank_Selenium/src/main/java/com/Arun/Practice/Logout_ParaBank.java
@@ -0,0 +1,10 @@
+package com.Arun.Practice;
+
+public class Logout_ParaBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
